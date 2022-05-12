a5d7ea3e3ba5f5eb546b39f38d93a12ba2ea3ca6
sweetautumndeer <78242996+sweetautumndeer@users.noreply.github.com>
2022-05-12 16:02:11 -0700

    Added Level1 Text

--- Src/Scenes/Level2.js
+++ Src/Scenes/Level2.js
@@ -3,4 +3,22 @@ class Level2 extends Phaser.Scene
     constructor() {
         super("Level2Scene");
     }
+    
+    create()
+    {
+        this.textConfig = {
+            fontFamily: 'Georgia',
+            fontSize: '40px',
+            //backgroundColor: '#AA11AA',
+            color: '#ED1C24',
+            align: 'right',
+            padding: {
+                top: 5,
+                bottom: 5,
+            },
+            fixedWidth: 0
+        }
+        this.text = this.add.text(game.config.width/2, game.config.height/2, "That's good work!", textConfig);
+        this.text = this.add.text(game.config.width/2, game.config.height/2, "Watch out for snails!", textConfig);
+    }
 }
