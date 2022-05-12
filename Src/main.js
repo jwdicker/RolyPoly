c740ea9c03af6a01be3d76b5816cd546967fa1bd
sweetautumndeer <78242996+sweetautumndeer@users.noreply.github.com>
2022-05-12 16:06:04 -0700

    Added Lose scene to main.js

--- Src/main.js
+++ Src/main.js
@@ -2,7 +2,7 @@ let config = {
     type: Phaser.CANVAS,
     width: 1366,
     height: 768,
-    scene: [ Menu, Level1, Level2, Level3, Victory ],
+    scene: [ Menu, Level1, Level2, Level3, Victory, Lose ],
     highscore: 0,
     currentScore: 0
 };
