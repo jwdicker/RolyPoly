b7069958b0d604338f83e082ae941bb474f0ead2
Jacob Dickerman <jwdicker@ucsc.edu>
2022-05-12 15:46:32 -0700

    Added menu scene

---
+++ Src/Scenes/menu.js
@@ -0,0 +1,44 @@
+class Menu extends Phaser.Scene {
+    constructor() {
+        super("MenuScene");
+    }
+    preload() {
+        this.load.image('mainmenu', 'assets/mainmenu.png');
+        this.load.image('snail', 'assets/snail.png');
+        this.load.image('rolypoly', 'assets/rolypoly.png');
+    }
+    create() {
+        // Add background
+        this.add.image(0, 0, 'mainmenu').setOrigin(0, 0);
+
+        // Add snail sprite
+        this.snail = this.add.sprite(175, 500, 'snail').setOrigin(0.5, 0.5);
+
+        // Add roly poly sprite
+        this.rolypoly = this.add.sprite(890, 500, 'rolypoly').setOrigin(0.5, 0.5);
+
+        // Add tween for snail
+        this.add.tween({
+            targets: [this.snail],
+            duration: 300,
+            x: this.snail.x + 5,
+            y: this.snail.y - 5,
+            angle: this.snail.angle - 10,
+            yoyo: true,
+            repeat: -1,
+            ease: 'Sinusoidal',
+        });
+
+        // Add tween for roly poly
+        this.add.tween({
+            targets: [this.rolypoly],
+            duration: 300,
+            x: this.rolypoly.x + 5,
+            y: this.rolypoly.y - 5,
+            angle: this.rolypoly.angle - 10,
+            yoyo: true,
+            repeat: -1,
+            ease: 'Sinusoidal',
+        });
+    }
+}
