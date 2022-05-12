7feba1361b0e8292feedbd4f4c706fa45cdaf754
chuciy <czhao48@ucsc.edu>
2022-05-12 16:02:53 -0700

    player moving left to right

--- Src/main.js
+++ Src/main.js
@@ -1,10 +1,42 @@
+
+class Playground extends Phaser.Scene {
+    constructor() {
+        super("playground");
+    }
+
+    preload(){
+        this.load.image("player", "Assets/rolypoly.png");
+    }
+
+    create(){
+        this.player = this.physics.add.sprite(8,0,'player').setOrigin(0,0).setCollideWorldBounds();
+
+        this.player.setGravityY(1);
+        this.player.setVelocityY(4000);
+
+        this.player.setVelocityX(100);
+
+
+
+    }
+    //this.ground = this.physics.add.group({immovable: true});
+    
+
+}
+
 let config = {
     type: Phaser.CANVAS,
     width: 1366,
     height: 768,
-    scene: [ Menu, Load, Play, EndScreen ],
+    scene: [ Playground ],
     highscore: 0,
-    currentScore: 0
+    currentScore: 0,
+    physics: {
+        default: 'arcade',
+        arcade: {
+            debug: false
+        }
+    }
 };
 
 // define game canvas
