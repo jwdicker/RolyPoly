b283eb005372ef2e5c16ae76f8da47e9aaafacf9
Jacob Dickerman <jwdicker@ucsc.edu>
2022-05-19 15:07:22 -0700

    Condensed play scenes

---
+++ src/scenes/Play.js
@@ -0,0 +1,73 @@
+class Level1 extends Phaser.Scene 
+{
+    constructor() 
+    {
+        super("Level1Scene");
+    }
+
+    create()
+    {
+        let menuConfig = {
+            fontFamily: 'Georgia',
+            fontSize: '40px',
+            //backgroundColor: '#AA11AA',
+            color: '#ED1C24',
+            align: 'right',
+            padding: {
+                top: 5,
+                bottom: 5,
+            },
+            fixedWidth: 0
+        }
+        this.add.text(game.config.width/2, game.config.height/2, 'Watch out for slugs', menuConfig).setOrigin(0.5);
+    }
+}
+
+class Level2 extends Phaser.Scene 
+{
+    constructor() {
+        super("Level2Scene");
+    }
+    
+    create()
+    {
+        this.textConfig = {
+            fontFamily: 'Georgia',
+            fontSize: '40px',
+            //backgroundColor: '#AA11AA',
+            color: '#ED1C24',
+            align: 'right',
+            padding: {
+                top: 5,
+                bottom: 5,
+            },
+            fixedWidth: 0
+        }
+        this.text = this.add.text(game.config.width/2, game.config.height/2, "That's good work!", textConfig).setOrigin(0.5);;
+        this.text2 = this.add.text(game.config.width/2, game.config.height/2, "Watch out for snails!", textConfig).setOrigin(0.5);;
+    }
+}
+
+class Level3 extends Phaser.Scene 
+{
+    constructor() {
+        super("Level3Scene");
+    }
+    
+    create()
+    {
+        let menuConfig = {
+            fontFamily: 'Georgia',
+            fontSize: '40px',
+            //backgroundColor: '#AA11AA',
+            color: '#ED1C24',
+            align: 'right',
+            padding: {
+                top: 5,
+                bottom: 5,
+            },
+            fixedWidth: 0
+        }
+        this.add.text(game.config.width/2, game.config.height/2, 'Watch out for slugs' +  this.game.config.highScore, menuConfig).setOrigin(0.5);
+    }
+}
