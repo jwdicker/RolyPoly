b8fc4b59a709341405903eed7b38b5e7db949763
Jacob Dickerman <jwdicker@ucsc.edu>
2022-05-19 15:03:23 -0700

    Merge compatability change

--- src/main.js
+++ src/main.js
@@ -1,10 +1,8 @@
-
-
 let config = {
     type: Phaser.CANVAS,
     width: 1366,
     height: 768,
-    scene: [ Play ],
+    scene: [Load, Menu, Play, Victory, Lose],
     highscore: 0,
     currentScore: 0,
     physics: {
@@ -12,8 +10,8 @@ let config = {
         arcade: {
             gravity: { y: 100 },
             debug: true
-        } 
-}
+        }
+    }
 }
 
 // define game canvas
