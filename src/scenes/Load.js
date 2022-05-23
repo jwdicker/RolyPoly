7a3ea8e89316685da612012e91f90ad63c463762
Jacob Dickerman <jwdicker@ucsc.edu>
2022-05-22 22:30:50 -0700

    (Hopefully) fixed loading errors

--- src/scenes/Load.js
+++ src/scenes/Load.js
@@ -8,13 +8,13 @@ class Load extends Phaser.Scene {
         this.load.image('mainmenu', 'assets/mainmenu.png');
         this.load.image('snail', 'assets/snail.png');
         this.load.image('rolypoly', 'assets/rolypoly.png');
-        this.load.image('handIcon', '/assets/hand_c.png');
-        this.load.image('circIcon', '/assets/circ_icon.png');
+        this.load.image('handIcon', 'assets/hand_c.png');
+        this.load.image('circIcon', 'assets/circ_icon.png');
 
         // Play scene
         this.load.image("player", "assets/rolypoly.png");
         this.load.image("slug", "assets/slug.png");
-        this.load.spritesheet('poly_anim', '/assets/rolypoly_animated.png', {frameWidth: 85, frameHeight: 70, startFrame: 0, endFrame: 11});
+        this.load.spritesheet('poly_anim', 'assets/rolypoly_animated.png', {frameWidth: 85, frameHeight: 70, startFrame: 0, endFrame: 11});
         this.load.image("background", "assets/background.png");
 
         // End scene
